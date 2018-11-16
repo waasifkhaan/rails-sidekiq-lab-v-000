1ce5c241eb210af9c5beb40e0cd462e4e3592731
Wasif Khan <waasif.khaan@gmail.com>
2018-11-16 19:36:50 +0000

    Done.

---
+++ app/workers/songs_worker.rb
@@ -0,0 +1,10 @@
+class SongsWorker
+  require 'csv'
+  include Sidekiq::Worker
+
+  def perform(leads_file)
+    CSV.foreach(leads_file,  headers: true) do |lead|
+      Song.create(title: lead[0], artist_name: lead[1])
+    end
+  end
+end
